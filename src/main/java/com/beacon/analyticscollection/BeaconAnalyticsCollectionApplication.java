8cd70aeadab2a4fc2d350c70bb63ecebb5006fcc
Jordan Martel <jordan.martel@cbtassociates.com>
2018-07-05 15:12:31 -0400

    sending async message to event hub, still needs exception dealt with separately

--- src/main/java/com/beacon/analyticscollection/BeaconAnalyticsCollectionApplication.java
+++ src/main/java/com/beacon/analyticscollection/BeaconAnalyticsCollectionApplication.java
@@ -2,12 +2,10 @@ package com.beacon.analyticscollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
 @EnableAsync
-@EnableFeignClients
+@SpringBootApplication
 public class BeaconAnalyticsCollectionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/beacon/analyticscollection/service/AnalyticsCollectionService.java
+++ src/main/java/com/beacon/analyticscollection/service/AnalyticsCollectionService.java
@@ -1,34 +1,46 @@
 package com.beacon.analyticscollection.service;
 
-import com.beacon.analyticscollection.exception.EventHubRequestFailedException;
 import com.beacon.analyticscollection.model.EventHubRequest;
 import com.beacon.analyticscollection.model.EventType;
-import com.beacon.analyticscollection.rest.EventHubClient;
-import feign.FeignException;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.gson.GsonBuilder;
+import com.microsoft.azure.eventhubs.EventData;
+import com.microsoft.azure.eventhubs.EventHubClient;
+import com.microsoft.azure.eventhubs.EventHubException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 public class AnalyticsCollectionService {
 
+    @Value("${eventhub.connection-string}")
+    private String connectionString;
 
-    @Autowired
-    private EventHubClient eventHubClient;
-
+    // async method will not wait to return to the controller. Exceptions will not be returned to the user, but should be dealt with
+    // separately
     @Async
-    public void forwardTrackedEvent(EventType actionType, String ipAddress, String username) throws EventHubRequestFailedException{
+    public void forwardTrackedEvent(EventType actionType, String ipAddress, String username) throws EventHubException, IOException {
 
-        EventHubRequest request = new EventHubRequest(actionType, ipAddress, username);
+        EventHubRequest payload = new EventHubRequest(actionType, ipAddress, username);
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        final EventHubClient eventHubClient = EventHubClient.createSync(connectionString, executorService);
+
         try {
-            eventHubClient.sendEvent(request);
-        }
 
-        catch (FeignException e) {
-            throw new EventHubRequestFailedException(e.getMessage());
-        }
+            byte[] payloadBytes = new GsonBuilder().create().toJson(payload).getBytes(Charset.defaultCharset());
+            EventData sendEvent = EventData.create(payloadBytes);
 
-        // make a web request to the event hub service
+            eventHubClient.sendSync(sendEvent);
+            System.out.println(Instant.now() + ": Send Complete...");
+        } finally {
+            eventHubClient.closeSync();
+            executorService.shutdown();
+        }
     }
-
 }
